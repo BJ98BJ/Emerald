fc7d96b0154d78f75767c702f89dd2a13a9b0db0
BJ98BJ <bj98bj@outlook.com>
2014-07-06 13:34:31 +0200

    1

---
+++ src/main/java/com/bj98bj/emerald/Emerald.java
@@ -0,0 +1,44 @@
+package com.bj98bj.emerald;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Björn on 06.07.2014.
+ */
+
+@Mod(modid = "Emerald", name = "Emarald", version = "1.7.2-1.0",canBeDeactivated = true)
+
+public class Emerald
+{
+    @Mod.Instance("Emerald")
+    public static Emerald instance;
+
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+}
