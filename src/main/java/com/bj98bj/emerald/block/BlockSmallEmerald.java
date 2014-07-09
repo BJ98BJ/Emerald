371eeac4985da79d78642b5e834ed25b39296fc4
BJ98BJ <bj98bj@outlook.com>
2014-07-09 11:48:59 +0200

    1

---
+++ src/main/java/com/bj98bj/emerald/block/BlockSmallEmerald.java
@@ -0,0 +1,9 @@
+package com.bj98bj.emerald.block;
+
+/**
+ * Created by Björn on 09.07.2014.
+ */
+public class BlockSmallEmerald
+{
+
+}
--- src/main/java/com/bj98bj/emerald/item/ItemEmeraldSword.java
+++ src/main/java/com/bj98bj/emerald/item/ItemEmeraldSword.java
@@ -1,5 +1,9 @@
 package com.bj98bj.emerald.item;
 
+import com.bj98bj.emerald.init.ModItems;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+
 /**
  * Created by Björn on 06.07.2014.
  */
@@ -8,7 +12,9 @@ public class ItemEmeraldSword extends  ItemEmerald {
     public ItemEmeraldSword() {
         super();
         this.setUnlocalizedName("emeraldSword");
-        setMaxStackSize(1);
-        setMaxDamage(100);
+        this.setMaxStackSize(1);
+        this.setMaxDamage(100);
+        this.setCreativeTab(CreativeTabs.tabCombat);
+        this.setDamage(new ItemStack(ModItems.emeraldsword, 1),5);
     }
 }
